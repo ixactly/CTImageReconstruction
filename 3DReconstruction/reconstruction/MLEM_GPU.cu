838f70b50bef71b96489167e7cbc1803b4a043ad
TomokiMori <mori-tomoki@g.ecc.u-tokyo.ac.jp>
2022-07-18 06:04:40 +0900

    pass build cuda program

---
+++ 3DReconstruction/reconstruction/MLEM_GPU.cu
@@ -0,0 +1,17 @@
+//
+// Created by tomokimori on 22/07/16.
+//
+
+#include "MLEM_GPU.cuh"
+
+void MLEM_CUDA::reconstruct(Volume<float> &sinogram, Volume<float> &voxel, const Geometry &geom, const int epoch, const int batch, Rotate dir) {
+
+}
+
+__global__ void forward() {
+
+}
+
+__global__ void backward() {
+
+}
---
+++ 3DReconstruction/reconstruction/MLEM_GPU.cuh
@@ -0,0 +1,18 @@
+//
+// Created by tomokimori on 22/07/16.
+//
+
+#ifndef INC_3DRECONSTRUCTION_MLEM_GPU_CUH
+#define INC_3DRECONSTRUCTION_MLEM_GPU_CUH
+
+#include "../util/Volume.h"
+#include "Geometry.h"
+
+class MLEM_CUDA {
+public :
+    MLEM_CUDA() = default;
+
+    void reconstruct(Volume<float> &sinogram, Volume<float> &voxel, const Geometry &geom, const int epoch, const int batch, Rotate dir)
+};
+
+#endif //INC_3DRECONSTRUCTION_MLEM_GPU_CUH
